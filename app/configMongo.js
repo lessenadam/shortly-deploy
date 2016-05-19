224bbf7bed1f4674fb877296ab712212e310fd7b
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 11:48:27 -0700

    Add initial files to support migration to MongoDB

--- app/configMongo.js
+++ app/configMongo.js
@@ -3,13 +3,61 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017/newTest';
 
 mongoose.connect(url);
 
 
 var db = mongoose.connection;
 
-db.once('open', function(){
-  console.log('made it to mongoose--------');
-});
+// db.once('open', function(){
+//   console.log('made it to mongoose--------');
+// });
+
+var userSchema = new Schema({
+  username: {type: String, required: true}, 
+  password: {type: String, required: true},
+},
+  {
+    timestamps: true
+  }
+);
+
+var urlSchema = new Schema({
+  url: {type: String, required: true}, 
+  baseUrl: {type: String, required: true},
+  code: {type: String, required: true},
+  title: {type: String, required: true},
+  visits: {type: Number, required: true}
+},
+  {
+    timestamps: true
+  }
+);
+
+
+exports.db = db;
+exports.userSchema = userSchema;
+
+
+
+// var url = mongoose.model('url', urlsSchema);
+
+// var insertUser = new user ({username: 'adam', password: 'lessen'});
+
+// var insertUrl = new url ({
+//   url: 'google',
+//   baseUrl: 'goog',
+//   code: '202',
+//   title: 'the googs', 
+//   visits: '0'
+// });
+
+// insertUrl.save(function (err) {
+//   if (err) {
+//     return console.log(err);
+//   } 
+//   console.log('Url saved!--------------'); 
+//   db.close();
+// });
+
---
+++ app/models/usersMongo.js
@@ -0,0 +1,53 @@
+var db = require('../configMongo');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+// get around this ?
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+db.userSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+    });
+};
+
+db.userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+var user = mongoose.model('user', db.userSchema);
+
+
+// var insertUser = new user ({
+//   username: 'tony',
+//   password: 'mypaswword',
+// });
+
+// insertUser.hashPassword();
+
+// insertUser.save(function (err) {
+//   if (err) {
+//     return console.log(err);
+//   } 
+//   console.log('Url saved!--------------'); 
+//   db.db.close();
+// });
+
+// new user({username: 'tony'}).comparePasswordfind({ type: this.type }
+var username = 'tony';
+var password1 = 'mypaswword';
+var password2 = 'paswword';
+
+
+user.find({username: username}, function(err, record) {
+  record[0].comparePassword(password2, function(val) {
+    console.log(val);
+  });
+});         
+
